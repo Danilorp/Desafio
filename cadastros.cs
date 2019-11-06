a0241c6ef72d122dd5b48533d871a7b90f8b95a3
Danilorp <57377166+Danilorp@users.noreply.github.com>
2019-11-06 12:24:01 -0300

    Add files via upload

---
+++ cadastros.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace desafio
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+
+        public string RA { get; set; }
+
+        public string CPF { get; set; }
+
+        public string Curso { get; set; }
+
+        public DateTime Nascimento { get; set; }
+
+
+    }
+}
